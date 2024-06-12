39221e44c121203e63faa374eb601b6c3016632c
gitcommitcode <rahulkmr5041@gmail.com>
2024-06-12 23:55:11 +0530

    responsive

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,17 +12,13 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Label } from "@/components/ui/label"
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { loginUserSchema } from "../formValidation";
-import { useLogin } from "@/lib/React-query/Mutation";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/components/Redux/store";
-import { logIn } from "@/components/Redux/Auth/auth-slice";
-interface LoginUser {
-  email: string;
-  password: string;
+import { createUserSchema } from "../formValidation";
+interface LoginUser{
+  email:string;
+  password:string;
 }
 export default function Page() {
   const [formData, setFormData] = useState<LoginUser>({
@@ -30,9 +26,6 @@ export default function Page() {
     password: "",
   });
 
-  const { mutateAsync: loginMutation, isPending: loadingResponse } = useLogin();
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isAuth);
-  const dispatch = useDispatch();
   const router = useRouter();
   const [errors, setErrors] = useState<any[]>([]);
 
@@ -42,114 +35,94 @@ export default function Page() {
       [e.target.id]: e.target.value,
     });
   };
-
-  if (isLoggedIn) return router.push("/dashboard");
-
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(formData);
+
     try {
-      const response = loginUserSchema.safeParse({
+      const response = createUserSchema.safeParse({
         email: formData.email,
         password: formData.password,
       });
 
       if (!response.success) {
+        console.log("zod")
         let errArr: any[] = [];
         const { errors: err } = response.error;
         for (var i = 0; i < err.length; i++) {
           errArr.push({ for: err[i].path[0], message: err[i].message });
         }
+        console.log(errArr[0].message);
         setErrors(errArr);
-      } else {
+      }else{
         try {
-          const res = await loginMutation(formData);
-          dispatch(
-            logIn({
-              accessToken: res?.data.accessToken,
-              refreshToken: res?.data.refreshToken,
-              id: res?.data.user._id,
-              email: res?.data.user.email,
-            })
-          );
-          router.push("/dashboard");
+          // const result = await newUser(formData);
+
+          // if(result?.status === 201){
+          //   router.push('/login')
+          // }
+          console.log(formData)
+
         } catch (error) {
           console.error("Error submitting form:", error);
         }
       }
+
     } catch (error) {
       console.error(error);
     }
   };
 
+
+
+
   return (
-    <Card className=" bg-[#1F1F1F]   w-full flex flex-col md:flex-row max-w-lg md:max-w-3xl lg:max-w-4xl absolute  shadow-xl rounded-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-auto min-h-[30vh] md:min-h-[63vh] lg:min-h-[75vh] ">
-      <div className="w-full md:w-1/2 lg:w-1/3  flex items-center justify-center flex-col text-center p-4 md:p-6 rounded-b-lg md:rounded-bl-none md:rounded-l-lg min-h-[30vh] md:min-h-[40vh] lg:min-h-[50vh]">
-        <CardHeader className="w-full ">
-          <CardTitle className="block text-left absolute top-[3.5rem] tracking-tight w-60 text-2xl text-white font-bold">
-            Welcome Back !
-          </CardTitle>
-          <CardDescription className="text-sm text-left absolute top-[7rem] w-64 text-white leading-5 tracking-wide my-6 mt-[25px] mb-[35px] font-light">
-            Sign In To Plan Your Financial Goal
-          </CardDescription>
-          <CardFooter className="absolute left-4 bottom-1 w-80 h-80  ">
-            <Image alt="" width={1000} height={1000} src="/bg.svg" />
-          </CardFooter>
-        </CardHeader>
-      </div>
-      <div className=" lg:w-96 absolute h-[32.5rem] w-96 right-5 top-5 bottom-5 bg-white rounded  p-4 md:p-6 flex flex-col items-center">
-        <CardContent className=" ">
-          <form className="w-full" onSubmit={handleSubmit}>
-            <div className="w-full items-center gap-4 justify-center flex flex-col">
-              <h1 className="block text-2xl text-[#141E30] font-bold">
-                Sign In
-              </h1>
-              <div className="flex flex-col space-y-1.5 w-full">
-                <Label htmlFor="email">Email</Label>
-
-                <Input
-                  id="email"
-                  className="w-full"
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-                <div className="mt-1 text-xs text-red-500">
-                  {errors.find((error) => error.for === "email")?.message}
-                </div>
-              </div>
-              <div className="flex flex-col space-y-1.5 w-full">
-                <Label htmlFor="password">Password</Label>
-
-                <Input
-                  id="password"
-                  className="w-full"
-                  value={formData.password}
-                  onChange={handleChange}
-                />
-
-                <div className=" text-[0.55rem] text-gray-500 w-80">
-                  <p>
-                    Use 8 or more character with a mix of letters, numbers &
-                    symbols
-                  </p>
-                </div>
-                <div className="mt-1 text-xs text-red-500">
-                  {errors.find((error) => error.for === "password")?.message}
-                </div>
-              </div>
+    <Card className="bg-[#1F1F1F] w-full flex flex-col md:flex-row max-w-lg md:max-w-3xl lg:max-w-3xl absolute shadow-xl rounded-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-auto min-h-[30vh] md:min-h-[63vh] lg:min-h-[75vh]">
+  <div className="w-full md:w-1/2 lg:w-1/3 flex items-center justify-center flex-col text-center p-4 md:p-6 rounded-b-lg md:rounded-bl-none md:rounded-l-lg min-h-[30vh] md:min-h-[40vh] lg:min-h-[50vh]">
+    <CardHeader className="w-full ">
+      <CardTitle className="block md:absolute text-left top-14 md:top-[3.5rem] tracking-tight w-full md:w-60 text-2xl text-white font-bold">
+        Welcome Back!
+      </CardTitle>
+      <CardDescription className="text-sm md:absolute text-left top-28 md:top-[7rem] w-full md:w-64 text-white leading-5 tracking-wide my-6 font-light">
+        Sign In To Plan Your Financial Goal
+      </CardDescription>
+      <CardFooter className="absolute hidden md:block left-4 bottom-1 w-80 h-80">
+        <Image alt="" width={1000} height={1000} src="/bg.svg" />
+      </CardFooter>
+    </CardHeader>
+  </div>
+  <div className="w-full md:w-1/2 lg:w-[45%] md:h-[58vh] lg:h-[70vh] lg:absolute md:absolute md:top-4 md:right-4 lg:right-4 lg:top-4 bg-white rounded p-4 md:p-6 flex flex-col items-center">
+    <CardContent className="w-full">
+      <form className="w-full" onSubmit={handleSubmit}>
+        <div className="w-full items-center gap-4 justify-center flex flex-col">
+          <h1 className="block text-2xl text-[#141E30] font-bold">Sign In</h1>
+          <div className="flex flex-col space-y-1.5 w-full">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" className="w-full" value={formData.email} onChange={handleChange} />
+            <div className="mt-1 text-xs text-red-500">
+              {errors.find((error) => error.for === "email")?.message}
             </div>
-            <CardFooter className="flex flex-col items-center justify-center gap-1 mt-4 ">
-              <p className="text-xs">
-                Create account
-                <Link className="text-blue-500" href="./register">
-                  Sign Up
-                </Link>
-              </p>
-              <Button type="submit">Sign In</Button>
-            </CardFooter>
-          </form>
-        </CardContent>
-      </div>
-    </Card>
+          </div>
+          <div className="flex flex-col space-y-1.5 w-full">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" className="w-full" value={formData.password} onChange={handleChange} />
+            <div className="text-[0.55rem] text-gray-500 w-full">
+              <p>Use 8 or more characters with a mix of letters, numbers & symbols</p>
+            </div>
+            <div className="mt-1 text-xs text-red-500">
+              {errors.find((error) => error.for === "password")?.message}
+            </div>
+          </div>
+        </div>
+        <CardFooter className="flex flex-col items-center justify-center gap-1 mt-4">
+          <Button type="submit">Sign In</Button>
+          <p className="text-xs">
+            Create account <Link className="text-blue-500" href="./register">Sign Up</Link>
+          </p>
+        </CardFooter>
+      </form>
+    </CardContent>
+  </div>
+</Card>
+
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -12,28 +12,32 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Label } from "@/components/ui/label"
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { createUserSchema } from "../formValidation";
-import { useSignIn } from "@/lib/React-query/Mutation";
-import { USER } from "@/types";
-import { useSelector } from "react-redux";
-import { RootState } from "@/components/Redux/store";
+// import { useSelector } from "react-redux";
+// import { RootState } from "@/Redux/store";
+interface USER{
+  firstName:string;
+  lastName:string;
+  phoneNumber:string;
+  email:string;
+  password:string;
+}
 export default function Page() {
   const [formData, setFormData] = useState<USER>({
-    firstname: "",
-    lastname: "",
-    phone: "",
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
     email: "",
     password: "",
   });
 
   const router = useRouter();
   const [errors, setErrors] = useState<any[]>([]);
-  const { mutateAsync: addNewUserMutation, isPending: loadingResponse } =
-    useSignIn();
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isAuth);
+  // const { mutateAsync: newUser, isPending: loadingResponse } = useSignIn();
+  // const isLoggedIn = useSelector((state:RootState)=>state.auth.isAuth);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -41,57 +45,68 @@ export default function Page() {
       [e.target.id]: e.target.value,
     });
   };
-
-  if (isLoggedIn) return router.push("/dashboard");
-
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
       const response = createUserSchema.safeParse({
-        firstname: formData.firstname,
-        lastname: formData.lastname,
+        firstName: formData.firstName,
+        lastName: formData.lastName,
         email: formData.email,
-        phone: formData.phone,
+        phoneNumber: formData.phoneNumber,
         password: formData.password,
       });
 
       if (!response.success) {
+        console.log("zod")
         let errArr: any[] = [];
         const { errors: err } = response.error;
         for (var i = 0; i < err.length; i++) {
           errArr.push({ for: err[i].path[0], message: err[i].message });
         }
+        console.log(errArr[0].message);
         setErrors(errArr);
-      } else {
+      }else{
         try {
-          const res = await addNewUserMutation(formData);
-          router.push("/login");
+          // const result = await newUser(formData);
+
+          // if(result?.status === 201){
+          //   router.push('/login')
+          // }
+          console.log(formData)
+
         } catch (error) {
           console.error("Error submitting form:", error);
         }
       }
+
     } catch (error) {
       console.error(error);
     }
   };
 
+
+
+
   return (
-    <Card className="bg-[#1F1F1F]   w-full flex flex-col md:flex-row max-w-lg md:max-w-3xl lg:max-w-4xl absolute  shadow-xl rounded-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-auto min-h-[30vh] md:min-h-[63vh] lg:min-h-[75vh] ">
-      <div className="w-full lg:w-1/3  flex items-center justify-center flex-col text-center p-4 md:p-6 rounded-b-lg md:rounded-bl-none md:rounded-l-lg  lg:min-h-[50vh]">
+    <Card className="bg-[#1F1F1F] w-full flex flex-col md:flex-row max-w-lg md:max-w-3xl lg:max-w-4xl absolute shadow-xl rounded-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-auto min-h-[30vh] md:min-h-[63vh] lg:min-h-[75vh] ">
+      <div className="w-full md:w-1/2 lg:w-1/3 flex items-center justify-center flex-col text-center p-4 md:p-6 rounded-b-lg md:rounded-bl-none md:rounded-l-lg min-h-[30vh] md:min-h-[40vh] lg:min-h-[50vh]">
         <CardHeader className="w-full ">
-          <CardTitle className="block text-left absolute top-[3.5rem] tracking-tight w-60 text-2xl text-white font-bold">
+          <CardTitle className="block md:absolute text-left top-14 md:top-[3.5rem] tracking-tight w-full md:w-60 text-2xl text-white font-bold">
             Plan Your Financial Goal with us
           </CardTitle>
-          <CardDescription className="text-sm text-left absolute top-[7rem] w-64 text-white leading-5 tracking-wide my-6 mt-[25px] mb-[35px] font-light">
+          <CardDescription className="text-sm md:absolute text-left top-28 md:top-[7rem] w-full md:w-64 text-white leading-5 tracking-wide my-6 font-light">
             To keep connected with us please login with your personal info
           </CardDescription>
-          <CardFooter className="absolute left-4 bottom-1 w-80 h-80  ">
-            <Image alt="" width={1000} height={1000} src="/bg.svg" />
+          <CardFooter className="absolute hidden md:block left-4 bottom-1 w-80 h-80">
+            <Image
+              alt="" width={1000} height={1000}
+              src="/bg.svg"
+            />
           </CardFooter>
         </CardHeader>
       </div>
-      <div className=" lg:w-96 absolute h-[32.5rem] w-96 right-5 top-5 bottom-5 bg-white rounded  p-4 md:p-6 flex flex-col items-center">
+      <div className="w-full md:w-1/2 lg:w-[45%] md:h-[58vh] lg:h-[70vh] lg:absolute md:absolute md:top-4 md:right-4 lg:right-4 lg:top-4 bg-white rounded p-4 md:p-6 flex flex-col items-center">
         <CardContent className=" ">
           <form className="w-full" onSubmit={handleSubmit}>
             <div className="w-full items-center gap-4 justify-center flex flex-col">
@@ -99,14 +114,15 @@ export default function Page() {
                 Sign Up Now
               </h1>
               <div className="grid grid-cols-2 gap-4">
+
                 <div className="flex flex-col space-y-1.5 w-full">
                   <Label htmlFor="name">First Name</Label>
 
                   <Input
-                    id="firstname"
+                    id="firstName"
                     className="w-full"
-                    value={formData.firstname}
-                    onChange={handleChange}
+                  value={formData.firstName}
+                  onChange={handleChange}
                   />
                   <div className="mt-1 text-xs text-red-500">
                     {errors.find((error) => error.for === "name")?.message}
@@ -116,72 +132,64 @@ export default function Page() {
                   <Label htmlFor="name">Last Name</Label>
 
                   <Input
-                    id="lastname"
+                    id="lastName"
                     className="w-full"
-                    value={formData.lastname}
-                    onChange={handleChange}
+                  value={formData.lastName}
+                  onChange={handleChange}
                   />
                   <div className="mt-1 text-xs text-red-500">
                     {errors.find((error) => error.for === "name")?.message}
                   </div>
                 </div>
-              </div>
-              <div className="flex flex-col space-y-1.5 w-full">
-                <Label htmlFor="email">Email</Label>
+                </div>
+                <div className="flex flex-col space-y-1.5 w-full">
+                  <Label htmlFor="email">Email</Label>
 
-                <Input
-                  id="email"
-                  className="w-full"
+                  <Input
+                    id="email"
+                    className="w-full"
                   value={formData.email}
                   onChange={handleChange}
-                />
-                <div className="mt-1 text-xs text-red-500">
-                  {errors.find((error) => error.for === "email")?.message}
+                  />
+                  <div className="mt-1 text-xs text-red-500">
+                    {errors.find((error) => error.for === "email")?.message}
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col space-y-1.5 w-full">
-                <Label htmlFor="number">Phone Number</Label>
+                <div className="flex flex-col space-y-1.5 w-full">
+                  <Label htmlFor="number">Phone Number</Label>
 
-                <Input
-                  id="phone"
-                  className="w-full"
-                  value={formData.phone}
+                  <Input
+                    id="phoneNumber"
+                    className="w-full"
+                  value={formData.phoneNumber}
                   onChange={handleChange}
-                />
-                <div className="mt-1 text-xs text-red-500">
-                  {errors.find((error) => error.for === "password")?.message}
+                  />
+                  <div className="mt-1 text-xs text-red-500">
+                    {errors.find((error) => error.for === "password")?.message}
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col space-y-1.5 w-full">
-                <Label htmlFor="password">Password</Label>
+                <div className="flex flex-col space-y-1.5 w-full">
+                  <Label htmlFor="password">Password</Label>
 
-                <Input
-                  id="password"
-                  className="w-full"
+                  <Input
+                    id="password"
+                    className="w-full"
                   value={formData.password}
                   onChange={handleChange}
-                />
+                  />
 
-                <div className=" text-[0.55rem] text-gray-500 w-80">
-                  <p>
-                    Use 8 or more character with a mix of letters, numbers &
-                    symbols
-                  </p>
-                </div>
-                <div className="mt-1 text-xs text-red-500">
-                  {errors.find((error) => error.for === "password")?.message}
+                  <div className=" text-[0.55rem] text-gray-500 w-80">
+                    <p>Use 8 or more character with a mix of letters, numbers & symbols</p>
+                  </div>
+                  <div className="mt-1 text-xs text-red-500">
+                    {errors.find((error) => error.for === "password")?.message}
+                  </div>
                 </div>
               </div>
-            </div>
-            <CardFooter className="flex flex-col items-center justify-center gap-1 mt-4 ">
-              <p className="text-xs">
-                Already have an account?
-                <Link className="text-blue-500" href="./login">
-                  Log in
-                </Link>
-              </p>
-              <Button type="submit">Sign Up</Button>
-            </CardFooter>
+              <CardFooter className="flex flex-col items-center justify-center gap-1 mt-4">
+                <Button type="submit">Sign Up</Button>
+                <p className="text-xs" >Already have an account? <Link className="text-blue-500" href="./login" >Log in</Link> </p>
+              </CardFooter>
           </form>
         </CardContent>
       </div>
