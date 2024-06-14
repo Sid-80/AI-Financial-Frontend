96620411d05b56c36b16396cc70701321336bd57
Sid-80 <siddharthbramhecha@gmail.com>
2024-06-14 23:37:44 +0530

    feat: retirement planning

--- app/dashboard/planning/page.tsx
+++ app/dashboard/planning/page.tsx
@@ -1,6 +1,29 @@
+import { columns } from "@/components/shared/Planning/Columns";
+import { DataTable } from "@/components/shared/Planning/DataTable";
+import SearchBar from "@/components/shared/Planning/SearchBar";
+import { RetirementPlanningFile } from "@/types";
 
 export default function Page() {
+  const data: RetirementPlanningFile[] = [
+    {
+      _id: "2123j31",
+      filename: "file1",
+      createdAt: "1/1/24",
+      updatedAt: "1/8/24",
+    },
+    {
+      _id: "2123j31fdf",
+      filename: "file2",
+      createdAt: "1/5/24",
+      updatedAt: "1/6/24",
+    },
+  ];
   return (
-    <div>Page</div>
-  )
+    <div className="flex-1 flex flex-col">
+      <SearchBar />
+      <div className="flex-1 p-5 text-white">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </div>
+  );
 }
